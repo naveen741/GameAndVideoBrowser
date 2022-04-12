5ad255d7917f33a6a378fc1299241d757bd7846a
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-12 18:39:00 +0530

    Add files via upload

---
+++ src/main/java/puzzlegame/MainGame.java
@@ -0,0 +1,40 @@
+package puzzlegame;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class MainGame {
+	/**
+	 * this is the main engine of our application 
+	 */
+	static Logger logger=Logger.getLogger(MainGame.class.getName());
+	public static void main(String[] args) throws IOException {
+		logger.log(Level.INFO,"==================GAMES AND APPLICATION==================");
+		Scanner sc=new Scanner(System.in);
+		logger.log(Level.INFO,"1.Tic_Tac_Toe \n2.Rearranging Number puzzle\n3.Video Browser\n4.Exit\nEnter your choice: ");
+		int choice;
+		boolean flag=true;
+		while(flag) {
+			choice=sc.nextInt();
+			switch(choice) {
+			case 1:
+				new TicTacToe();
+				break;
+			case 2:
+				new ArrangeNumber();
+				break;
+			case 3:
+				new VideoBrowser();
+				break;
+			case 4:
+				flag=false;
+				break;
+			default:
+				logger.log(Level.INFO,"Enter a valid choice.");
+			}
+		}
+		sc.close();
+	}
+
+}
---
+++ src/main/java/puzzlegame/TicTacToe.java
@@ -0,0 +1,162 @@
+package puzzlegame;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+
+
+public class TicTacToe extends Frame implements ActionListener{
+	private static final long serialVersionUID = 1L;
+	Button p1;
+	Button p2; 
+	Button p3;
+	Button p4; 
+	Button p5;
+	Button p6;
+	Button p7;
+	Button p8;
+	Button p9;
+	Button reStart;
+	Button exit;
+	int playerTurn=1;
+	String winner="noWin";
+	TicTacToe(){
+		super("Tic_Tac_Toe(X-O Game)");
+		p1=new Button("");
+		p1.setBounds(100, 50, 100, 100);
+		p2=new Button("");
+		p2.setBounds(200, 50,100, 100);
+		p3=new Button("");
+		p3.setBounds(300, 50, 100, 100);
+		p4=new Button("");
+		p4.setBounds(100, 150, 100, 100);
+		p5=new Button("");
+		p5.setBounds(200, 150,100, 100);
+		p6=new Button("");
+		p6.setBounds(300, 150, 100, 100);
+		p7=new Button("");
+		p7.setBounds(100, 250, 100, 100);
+		p8=new Button("");
+		p8.setBounds(200, 250,100, 100);
+		p9=new Button("");
+		p9.setBounds(300, 250, 100, 100);
+		reStart=new Button("Restart");
+		reStart.setBounds(100, 350, 300, 75);
+		exit=new Button("Exit");
+		exit.setBounds(100, 425, 300, 75);
+		p1.setBackground(Color.cyan);
+		p3.setBackground(Color.cyan);
+		p5.setBackground(Color.cyan);
+		p7.setBackground(Color.cyan);
+		p9.setBackground(Color.cyan);
+		p2.setBackground(Color.blue);
+		p4.setBackground(Color.blue);
+		p6.setBackground(Color.blue);
+		p8.setBackground(Color.blue);
+		p1.addActionListener(this);
+		p2.addActionListener(this);
+		p3.addActionListener(this);
+		p4.addActionListener(this);
+		p5.addActionListener(this);
+		p6.addActionListener(this);
+		p7.addActionListener(this);
+		p8.addActionListener(this);
+		p9.addActionListener(this);
+		reStart.addActionListener(this);
+		exit.addActionListener(this);
+		add(p1);add(p2);add(p3);add(p4);add(p5);add(p6);add(p7);add(p8);add(p9);
+		add(reStart);add(exit);
+		setBackground(Color.white);
+		setFont(new Font("Serif", Font.BOLD, 50));
+		setSize(500,500);  
+	    setLayout(null);  
+	    setVisible(true);  
+	    setResizable(false);
+	}
+	public void actionPerformed(ActionEvent e) {
+		if(((Button) e.getSource()).getLabel().equals("Restart"))
+			clrArea();
+		else if(((Button) e.getSource()).getLabel().equals("Exit"))
+			dispose();
+		else if(((Button) e.getSource()).getLabel().equals("")) {
+			if(playerTurn==1) {
+				((Button) e.getSource()).setLabel("X");
+				playerTurn=2;
+			}
+			else {
+				((Button) e.getSource()).setLabel("O");
+				playerTurn=1;
+			}
+		}
+		if(rowCheck() || colCheck() || diaCheck() || isFull())
+			showResult();
+	}
+	void showResult() {
+		if(winner.equals("X"))
+			JOptionPane.showMessageDialog(this,"Congratulations! X  won.");
+		else if(winner.equals("O"))
+			JOptionPane.showMessageDialog(this,"Congratulations! O  won.");
+		else
+			JOptionPane.showMessageDialog(this,"Match Draw.");
+		winner="noWin";
+		playerTurn=1;
+		clrArea();
+	}
+	boolean rowCheck() {
+		if((!p1.getLabel().equals("") && p1.getLabel().equals(p2.getLabel()) && p1.getLabel().equals(p3.getLabel()))){
+			winner=p1.getLabel();
+			return true;
+		}
+		if (!p4.getLabel().equals("") && p4.getLabel().equals(p5.getLabel()) && p4.getLabel().equals(p6.getLabel())){
+			winner=p4.getLabel();
+			return true;
+		}
+		if (!p7.getLabel().equals("") && p7.getLabel().equals(p8.getLabel()) && p7.getLabel().equals(p9.getLabel())){
+			winner=p7.getLabel();
+			return true;
+		}
+		return false;
+	}
+	boolean colCheck() {
+		if((!p1.getLabel().equals("") && p1.getLabel().equals(p4.getLabel()) && p1.getLabel().equals(p7.getLabel()))){
+			winner=p1.getLabel();
+			return true;
+		}
+		if (!p2.getLabel().equals("") && p2.getLabel().equals(p5.getLabel()) && p2.getLabel().equals(p8.getLabel())){
+			winner=p2.getLabel();
+			return true;
+		}
+		if (!p3.getLabel().equals("") && p3.getLabel().equals(p6.getLabel()) && p3.getLabel().equals(p9.getLabel())){
+			winner=p3.getLabel();
+			return true;
+		}
+		return false;
+	}
+	boolean diaCheck() {
+		if (!p1.getLabel().equals("") && p1.getLabel().equals(p5.getLabel()) && p1.getLabel().equals(p9.getLabel())){
+			winner=p1.getLabel();
+			return true;
+		}
+		if (!p3.getLabel().equals("") && p3.getLabel().equals(p5.getLabel()) && p3.getLabel().equals(p7.getLabel())){
+			winner=p3.getLabel();
+			return true;
+		}
+		return false;
+	}
+	boolean isFull() {
+		 return !(p1.getLabel().equals("") || p2.getLabel().equals("") || p3.getLabel().equals("")
+				|| p4.getLabel().equals("") || p5.getLabel().equals("") || p6.getLabel().equals("")
+					|| p7.getLabel().equals("") || p8.getLabel().equals("") || p9.getLabel().equals(""));
+	}
+	void clrArea() {
+		p1.setLabel("");
+		p2.setLabel("");
+		p3.setLabel("");
+		p4.setLabel("");
+		p5.setLabel("");
+		p6.setLabel("");
+		p7.setLabel("");
+		p8.setLabel("");
+		p9.setLabel("");
+	}
+}
