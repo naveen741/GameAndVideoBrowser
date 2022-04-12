ad783bb964b94b37d9a9f065f416bd9133a4c0fb
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-12 18:17:36 +0530

    Update MainGame.java

--- src/main/java/puzzlegame/MainGame.java
+++ src/main/java/puzzlegame/MainGame.java
@@ -2,14 +2,17 @@ package puzzlegame;
 
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class MainGame {
-
+	/**
+	 * this is the main engine of our application 
+	 */
+	static Logger logger=Logger.getLogger(MainGame.class.getName());
 	public static void main(String[] args) throws IOException {
-		System.out.println("==================GAMES AND APPLICATION==================");
+		logger.log(Level.INFO,"==================GAMES AND APPLICATION==================");
 		Scanner sc=new Scanner(System.in);
-		System.out.println("1.Tic_Tac_Toe \n2.Rearranging Number puzzle\n3.Video Browser\n4.Exit");
-		System.out.println("Enter your choice: ");
+		logger.log(Level.INFO,"1.Tic_Tac_Toe \n2.Rearranging Number puzzle\n3.Video Browser\n4.Exit\nEnter your choice: ");
 		int choice;
 		boolean flag=true;
 		while(flag) {
@@ -22,13 +25,13 @@ public class MainGame {
 				new ArrangeNumber();
 				break;
 			case 3:
-				new MediaPlayer();
+				new VideoBrowser();
 				break;
 			case 4:
 				flag=false;
 				break;
 			default:
-				System.out.println("Enter a valid choice.");
+				logger.log(Level.INFO,"Enter a valid choice.");
 			}
 		}
 		sc.close();
